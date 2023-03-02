389ca83a5006fa9edc89b39f58def3f9b2f6e425
gagankumar03 <gagankumar03@nagarro.com>
2023-03-02 09:15:30 +0530

    charging station

--- evStation/src/main/java/com/charging/station/repository/ChargingStationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.charging.station.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.charging.station.model.ChargingStation;
-
-public interface ChargingStationRepository extends JpaRepository<ChargingStation, Long> {
-}
